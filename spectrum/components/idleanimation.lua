ed52965926e69b0402499cea550dc345f39ac5cc
LJNIC <liamreadsspam@gmail.com>
2025-09-19 19:27:55 -0300

    Allow idle animations and drop tables to be serialized

--- spectrum/components/idleanimation.lua
+++ spectrum/components/idleanimation.lua
@@ -1,10 +1,16 @@
 --- @class IdleAnimation : Component
+--- @field animationName string
 --- @field animation Animation
---- @overload fun(animation: Animation): IdleAnimation
+--- @overload fun(animationName: string): IdleAnimation
 local IdleAnimation = prism.Component:extend "IdleAnimation"
 
-function IdleAnimation:__new(animation)
-   self.animation = animation
+function IdleAnimation:__new(animationName)
+   self.animationName = animationName
+   self.animation = spectrum.animations[animationName]()
+end
+
+function IdleAnimation:onDeserialize()
+   self.animation = spectrum.animations[self.animationName]()
 end
 
 return IdleAnimation
