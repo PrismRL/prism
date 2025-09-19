347ca08a0b21803305d3544ae5c8482c9b079bde
LJNIC <liamreadsspam@gmail.com>
2025-09-19 19:28:34 -0300

    Migrate Drawable completely to Sprite table

--- spectrum/components/drawable.lua
+++ spectrum/components/drawable.lua
@@ -3,40 +3,18 @@
 --- @field color Color4
 --- @field background Color4
 --- @field size integer
+--- @field indices? Sprite[]
 --- @overload fun(index: string|integer|Sprite, color: Color4?, background: Color4?, layer: number?, size: integer?): Drawable
 local Drawable = prism.Component:extend "Drawable"
 
-local warned = false
-
---- Index needs to be a string associated with a sprite in the SpriteAtlas, or
---- an integer index associated with a sprite.
---- @param index string|integer|Sprite
---- @param color Color4?
---- @param background Color4?
---- @param layer integer?
---- @param size integer?
-function Drawable:__new(index, color, background, layer, size)
-   if type(index) == "table" then
-      local sprite = index
-      self.index = sprite.index
-      self.color = sprite.color or prism.Color4.WHITE
-      self.background = sprite.background or prism.Color4.TRANSPARENT
-      self.layer = sprite.layer or 1
-      self.size = sprite.size or 1
-      return
-   elseif not warned then
-      warned = true
-      prism.logger.warn(
-         "Drawable now uses an option table, the multi-argument constructor",
-         "has been deprecated and will be removed upon release 1!"
-      )
-   end
-
-   self.index = index
-   self.color = color or prism.Color4.WHITE
-   self.background = background or prism.Color4.TRANSPARENT
-   self.layer = layer or 1
-   self.size = size or 1
+--- @param sprite Sprite
+function Drawable:__new(sprite)
+   self.index = sprite.index
+   self.color = sprite.color or prism.Color4.WHITE
+   self.background = sprite.background or prism.Color4.TRANSPARENT
+   self.layer = sprite.layer or 1
+   self.size = sprite.size or 1
+   self.indices = sprite.indices
 end
 
 return Drawable
