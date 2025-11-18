25d51065befc283e83ff6605f22060a9e2486d42
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-11-18 13:44:43 -0500

    Added missing overload doc comments, thanks templar!

--- extra/sight/components/sight.lua
+++ extra/sight/components/sight.lua
@@ -1,6 +1,11 @@
+--- @class SightOptions
+--- @field range integer
+--- @field fov boolean
+
 --- @class Sight : Component
 --- @field range integer How many tiles can this actor see?
 --- @field fov boolean
+--- @overload fun(options: SightOptions): Sight
 local Sight = prism.Component:extend("Sight")
 Sight.requirements = { "Senses" }
 
@@ -8,7 +13,7 @@ function Sight:getRequirements()
    return prism.components.Senses
 end
 
---- @param options {range: integer, fov: boolean}
+--- @param options SightOptions
 function Sight:__new(options)
    self.range = options.range
    self.fov = options.fov
