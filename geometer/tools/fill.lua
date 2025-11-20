0774f2e2b760d39f58849dcce2594ea114138914
LJNIC <liamreadsspam@gmail.com>
2025-11-20 19:45:08 -0400

    geometer: minor improvement to fill tool

--- geometer/tools/fill.lua
+++ geometer/tools/fill.lua
@@ -26,18 +26,17 @@ function Fill:bucket(attachable, x, y)
    local cell = attachable:getCell(x, y)
 
    prism.BreadthFirstSearch(prism.Vector2(x, y), function(searchX, searchY)
+      local cellAt
       if prism.LevelBuilder:is(attachable) then
          --- @cast attachable LevelBuilder
          -- get the raw value, since LevelBuilder returns the default cell
-         local cellAt = prism.SparseGrid.get(attachable, searchX, searchY)
-         if not cellAt then return false end
+         cellAt = prism.SparseGrid.get(attachable, searchX, searchY)
       else
-         --- @cast attachable Level
-         local cellAt = attachable:getCell(searchX, searchY)
-         if not cellAt then return false end
+         cellAt = attachable:getCell(searchX, searchY)
       end
+      if not cellAt then return false end
 
-      local cellAt = attachable:getCell(searchX, searchY)
+      cellAt = attachable:getCell(searchX, searchY)
       return cellAt:getName() == cell:getName()
    end, function(setX, setY)
       self.locations:set(setX, setY, true)
