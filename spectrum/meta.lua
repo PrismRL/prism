935317a4b339415dd9c4d6bb6f5b276fa4ee144c
LJNIC <liamreadsspam@gmail.com>
2025-11-27 17:42:31 -0400

    Generate factory aliases for registries

--- spectrum/meta.lua
+++ spectrum/meta.lua
@@ -1,13 +1,5 @@
 --- @meta
 
---- Animations registry.
-spectrum.animations = {}
-
---- Registers an animation in the animations registry.
---- @param name string
---- @param factory AnimationFactory
-function spectrum.registerAnimation(name, factory) end
-
 ---
 ---@class GameState
 local GameState = nil
